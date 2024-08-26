a2b4a541308c4595ec9487998096ea5c52b3df0f
kine14 <74851601+zeuxi141@users.noreply.github.com>
2024-08-26 21:01:50 +0700

    Build logic  Wallet details

--- MyEtherWallet/src/components/WalletVeiw.jsx
+++ MyEtherWallet/src/components/WalletVeiw.jsx
@@ -1,17 +1,14 @@
-import React from "react";
 import {
-  Divider,
-  Tooltip,
-  List,
   Avatar,
-  Spin,
+  Divider,
+  List,
   Tabs,
-  Input,
-  Button,
-} from "antd"
+  Tooltip
+} from "antd";
+import React from "react";
 
-import { LogoutOutlined } from "@ant-design/icons"
-import { useNavigate } from "react-router-dom"
+import { LogoutOutlined } from "@ant-design/icons";
+import { useNavigate } from "react-router-dom";
 
 
 
@@ -51,6 +48,82 @@ function WalletView({wallet, setWallet, seedPhrase, setSeedPhrase, selectedChain
 
   const navigate = useNavigate()
 
+  const items = [
+    {
+      key: "3",
+      label: "Tokens",
+      children: (
+        <>
+          {tokens ? (
+            <>
+              <List
+                bordered
+                itemLayout="horizontal"
+                dataSource={tokens}
+                renderItem={(item) => (
+                  <List.Item>
+                    <List.Item.Meta
+                      avatar={<Avatar>{item.symbol}</Avatar>}
+                      title={item.name}
+                      description={`Balance: ${Number(item.balance)/10 ** Number(item.decimals).toFixed(2)} Tokens`}
+                    />
+                  </List.Item>
+                )}
+              />
+            </>
+          ):(
+            <>
+              <span>You seem to not have any tokens yet</span>
+              <p>Would you like to add some?
+                <a href="" className="">Get faucet here</a>
+              </p>
+            </>
+          )}
+        </>
+      ),
+    },
+    {
+      key: "2",
+      label: "NFTs",
+      children: (
+        <>
+          {NFTs ? (
+            <>
+              {NFTs.map((nft, i) => {
+                return (
+                  <>
+                    {e && (
+                      <img 
+                        key={i}
+                        className="nftImage"
+                        alt="NFT"
+                        src={e}
+                      />
+                    )}
+                  </>
+                )
+              })}
+            </>
+          ):(
+            <>
+              <List.Item>
+              </List.Item>
+            </>
+            )}
+        </>
+      ),
+    },
+    {
+      key: "1",
+      label: "Transfer",
+      children: (
+        <>
+          Transfer
+        </>
+      ),
+    }
+  ]
+
   function logout() {
     setWallet(null)
     setSeedPhrase(null)
@@ -63,7 +136,16 @@ function WalletView({wallet, setWallet, seedPhrase, setSeedPhrase, selectedChain
         <div className="logoutButton" onClick={logout}>
           <LogoutOutlined />
         </div>
-        {wallet}
+        <div className="walletName">
+          Wallet
+        </div>
+        <Tooltip title={`Wallet address: ${wallet}`}>
+          <div>
+            {wallet.slice(0, 4)}...{wallet.slice(-4)}
+          </div>
+        </Tooltip>
+        <Divider />
+        <Tabs defaultActiveKey="1" items={items} className="walletVeiw"></Tabs>
       </div>
     </div>
   );
